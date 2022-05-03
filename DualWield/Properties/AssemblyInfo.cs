4fa44cd753a0bf9d93622366b70aef174cec597e
blaxxun <blaxxun@blaxxun.blaxxun>
2022-05-04 01:41:05 +0200

    small maintenance

--- DualWield/Properties/AssemblyInfo.cs
+++ DualWield/Properties/AssemblyInfo.cs
@@ -5,7 +5,7 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("DualWield")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("https://valheim.thunderstore.io/package/Smoothbrain/DualWield")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("DualWield")]
